59c1d922c95112d29a5860fb08838811f3e77780
Hiroki Takahashi <htakahashi@hyphenio.com.au>
2017-12-02 18:07:43 +1000

    Completed the views

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,16 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Login - Hiroki's FB App</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-            </div>
-        </div>
+        <a href="/login">Click and login with Facebook</a>
     </body>
 </html>
