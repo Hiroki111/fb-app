87f94f39a883970de754b0285561e2041e041300
Hiroki Takahashi <htakahashi@hyphenio.com.au>
2017-12-02 14:49:13 +1000

    Fixed config

--- config/app.php
+++ config/app.php
@@ -227,9 +227,4 @@ return [
         'View'         => Illuminate\Support\Facades\View::class,
 
     ],
-    'config'          => [
-        'app_id'                => env('FACEBOOK_APP_ID'),
-        'app_secret'            => env('FACEBOOK_APP_SECRET'),
-        'default_graph_version' => env('FACEBOOK_DEFAULT_GRAPH_VERSION'),
-    ],
 ];
